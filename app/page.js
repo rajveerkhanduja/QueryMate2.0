41604993db8e82b24dd46889cead57eee28cae1b
Rajveer Singh Khanduja <128909262+rajveerkhanduja@users.noreply.github.com>
2024-08-06 20:30:14 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <h2>Welcome To QueryMate.<br/>Home Page under development.</h2>
-      <Button>Add (/dashboard) to the URL</Button>
+      <Button>Add (/sign-in) to the URL</Button>
     </div>
   );
 }
