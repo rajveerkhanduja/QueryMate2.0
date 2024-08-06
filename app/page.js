20c663e2e88791932ae750efbb86c659b9fd0635
rajveerkhanduja <rajveerkhanduja74@gmail.com>
2024-08-06 22:16:47 +0530

    Resolved issue

--- app/page.js
+++ app/page.js
@@ -5,11 +5,7 @@ export default function Home() {
   return (
     <div>
       <h2>Welcome To QueryMate.<br/>Home Page under development.</h2>
-<<<<<<< HEAD
       <Button>Add (/dashboard) to the URL</Button>
-=======
-      <Button>Add (/sign-in) to the URL</Button>
->>>>>>> 41604993db8e82b24dd46889cead57eee28cae1b
     </div>
   );
 }
