17a1dfcd491b17df93f03d741bbf0271035a53b5
Rajveer Singh Khanduja <128909262+rajveerkhanduja@users.noreply.github.com>
2024-08-06 20:04:49 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h2>Welcome To QueryMate.<br>Home Page under development.</h2><br>
+      <h2>Welcome To QueryMate.<br>Home Page under development.</h2>
       <Button>Add (/dashboard) to the URL</Button>
     </div>
   );
